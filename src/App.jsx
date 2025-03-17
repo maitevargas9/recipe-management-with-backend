44a6913acf98ae5293eea1ddf2d97e69bd18c11e
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-03-17 10:32:47 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import RecipeManagementLogo from "/src/assets/recipe-management.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <img src={RecipeManagementLogo} alt="Recipe Management Logo" />
+      <h1>Recipe Management</h1>
+    </div>
+  );
+}
+
+export default App;
